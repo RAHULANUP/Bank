bb4eb1a3fc58ac70fa0f171b278f673e6448ce7a
Rahul Anup Varma <98415824+RAHULANUP@users.noreply.github.com>
2025-09-30 00:33:23 +0530

    Update CustomerControllerTest.java

--- src/test/java/com/bank/Bank/controller/CustomerControllerTest.java
+++ src/test/java/com/bank/Bank/controller/CustomerControllerTest.java
@@ -15,7 +15,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.*;
 
-class CustomerControllerUnitTest {
+class CustomerControllerTest {
     @Mock
     private AccountService accountService;
     @InjectMocks
@@ -28,18 +28,18 @@ class CustomerControllerUnitTest {
 
     @Test
     void withdrawFromCustomer_shouldReturnOk() {
-        when(accountService.withdrawFromCustomerAccount(anyLong(), any(BigDecimal.class)))
+        when(accountService.withdrawFromCustomerAccount(anyLong(), any(BigDecimal.class), null))
                 .thenReturn(new TransactionDto());
-        ResponseEntity<TransactionDto> response = customerController.withdrawFromCustomer(1L, BigDecimal.TEN);
+        ResponseEntity<TransactionDto> response = customerController.withdrawFromCustomer(1L, BigDecimal.TEN, null);
         assertEquals(200, response.getStatusCode().value());
         assertNotNull(response.getBody());
     }
 
     @Test
     void depositToCustomer_shouldReturnOk() {
-        when(accountService.depositToCustomerAccount(anyLong(), any(BigDecimal.class)))
+        when(accountService.depositToCustomerAccount(anyLong(), any(BigDecimal.class), any()))
                 .thenReturn(new TransactionDto());
-        ResponseEntity<TransactionDto> response = customerController.depositToCustomer(1L, BigDecimal.TEN);
+        ResponseEntity<TransactionDto> response = customerController.depositToCustomer(1L, BigDecimal.TEN, null);
         assertEquals(200, response.getStatusCode().value());
         assertNotNull(response.getBody());
     }
