adc8440517824fda30bd926b5ace8fa7ae6c9eaa
Rahul Anup Varma <98415824+RAHULANUP@users.noreply.github.com>
2025-09-30 00:25:07 +0530

    Update TransactionController.java

--- src/main/java/com/bank/Bank/controller/TransactionController.java
+++ src/main/java/com/bank/Bank/controller/TransactionController.java
@@ -1,5 +1,6 @@
 package com.bank.Bank.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -17,6 +18,7 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/api/transaction")
 @RequiredArgsConstructor
@@ -30,8 +32,8 @@ public class TransactionController {
                     @Content(schema = @Schema(implementation = Transaction.class)) }, responseCode = "201")
     })
     @GetMapping("/by-account")
-    public ResponseEntity<List<TransactionDto>> getTransactionsByAccount(@RequestParam Long accountId) {
-        List<TransactionDto> transactions = transactionService.getTransactionsByAccountId(accountId);
+    public ResponseEntity<List<TransactionDto>> getTransactionsByAccount(@RequestParam Long accountNumber) {
+        List<TransactionDto> transactions = transactionService.getTransactionsByAccountNumber(accountNumber);
         return ResponseEntity.ok(transactions);
     }
 }
