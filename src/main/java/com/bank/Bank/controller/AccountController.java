af4d9b42e7f249b560d2e43e3a12fe3bd42ea81c
Rahul Anup Varma <98415824+RAHULANUP@users.noreply.github.com>
2025-09-30 00:24:30 +0530

    Update AccountController.java

--- src/main/java/com/bank/Bank/controller/AccountController.java
+++ src/main/java/com/bank/Bank/controller/AccountController.java
@@ -3,6 +3,7 @@ package com.bank.Bank.controller;
 import java.math.BigDecimal;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -22,45 +23,37 @@ import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.RequiredArgsConstructor;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/api/account")
 @RequiredArgsConstructor
 public class AccountController {
-    private final AccountService accountService;
+        private final AccountService accountService;
 
-    @Operation(
-        summary="Get the account information",
-        description="Finds the first account information",
-        tags={"Get account info"}
-    )
-    @ApiResponses({
-    @ApiResponse(
-        description="200 OK",
-        content={@Content(schema=@Schema(implementation=Account.class))},
-        responseCode="200")
-    })
-    @GetMapping("/{accountNumber}")
-    public ResponseEntity<AccountDto> getFirstAccountInfo(@PathVariable("accountNumber") Long accountNumber) {
-        AccountDto dto = accountService.getFirstAccountInfo(accountNumber);
-        return ResponseEntity.ok(dto);
-    }
-    @Operation(
-        summary="Transfer from one account to another",
-        description="Transfer from one account to another on the basis of account Id",
-        tags={"Transfer"}
-    )
-    @ApiResponses({
-    @ApiResponse(
-        description="200 OK",
-        content={@Content(schema=@Schema(implementation=Account.class))},
-        responseCode="200")
-    })
-    @PostMapping("/transfer")
-    public ResponseEntity<TransactionDto> transfer(
-            @RequestParam Long fromAccountId,
-            @RequestParam Long toAccountId,
-            @RequestParam BigDecimal amount) {
-        TransactionDto dto = accountService.transferBetweenAccounts(fromAccountId, toAccountId, amount);
-        return ResponseEntity.ok(dto);
-    }
+        @Operation(summary = "Get the account information", description = "Finds the first account information", tags = {
+                        "Get account info" })
+        @ApiResponses({
+                        @ApiResponse(description = "200 OK", content = {
+                                        @Content(schema = @Schema(implementation = Account.class)) }, responseCode = "200")
+        })
+        @GetMapping("/{customerId}")
+        public ResponseEntity<AccountDto> getFirstAccountInfo(@PathVariable("customerId") Long customerId) {
+                AccountDto dto = accountService.getFirstAccountInfo(customerId);
+                return ResponseEntity.ok(dto);
+        }
+
+        @Operation(summary = "Transfer from one account to another", description = "Transfer from one account to another on the basis of account Id", tags = {
+                        "Transfer" })
+        @ApiResponses({
+                        @ApiResponse(description = "200 OK", content = {
+                                        @Content(schema = @Schema(implementation = Account.class)) }, responseCode = "200")
+        })
+        @PostMapping("/transfer")
+        public ResponseEntity<TransactionDto> transfer(
+                        @RequestParam Long fromAccountNumber,
+                        @RequestParam Long toAccountNumber,
+                        @RequestParam BigDecimal amount) {
+                TransactionDto dto = accountService.transferBetweenAccounts(fromAccountNumber, toAccountNumber, amount);
+                return ResponseEntity.ok(dto);
+        }
 }
